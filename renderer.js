5056eaa914cdf34e3466854b82784834643f1188
mkery <mkery@cs.cmu.edu>
2018-01-29 23:50:17 -0500

    working dark scene

--- renderer.js
+++ renderer.js
@@ -16,6 +16,8 @@ ctx.strokeStyle = pattern;
 ctx.lineWidth = 3;
 ctx.clearRect(0, 0, 1000, 1000);
 
+const milkboy = new Actor(ctx, './images/test_1.svg', 0)
+
 
 function wait(ms) {
   return new Promise(r => setTimeout(r, ms));
@@ -43,8 +45,12 @@ async function titleSequence(){ loopBuddy(() => {
     loopBuddy(() => {
       ctx.resetTransform();
       ctx.clearRect(0, 0, 1000, 1000);
+      ctx.beginPath();
+      ctx.fillStyle = 'red';
+      ctx.arc(400, 400, 100, 0, Math.PI * 2, true); // Outer circle
+      ctx.fill();
       title.draw(200,200)
-      title.mutate(titleEnd)
+      title.flail()
     }, 5000, 100).then( () => {
       ctx.resetTransform();
       ctx.clearRect(0, 0, 1000, 1000);
@@ -63,58 +69,97 @@ async function titleSequence(){ loopBuddy(() => {
 async function momSequence(){
   timeout(null, 500)
   //Mom enters
-  var milkboy = new Actor(ctx, './images/test_1.svg', 0)
   var mom = new Actor(ctx, './images/test_2.svg', 5)
   mom.loop = true
+  mom.clearOnDraw = true
   milkboy.loop = true
   mom.draw(70, 200)
-  mom.say("MILK BOY!!!", 35, 230, 150).then( () => {
+  mom.say("MILK JR!!!", 35, 230, 150).then( () => {
     timeout(() => milkboy.draw(550, 0), 200)
-    milkboy.beam(300, 0, 150, 300).then(() => {
-      timeout(null, 500).then( () => { ctx.resetTransform(); ctx.clearRect(0, 0, 1000, 1000); })
+    milkboy.beam(300, 0, 150, 200).then(() => {
+      timeout(null, 500).then( () => { ctx.resetTransform(); ctx.clearRect(500, 0, 1000, 1000); })
       mom.say("YOU ARE A MESS.", 35, 280, 50).then(() => {
         timeout(null, 2000).then(() => {
           milkboy.loop = false
           milkboy.removeBeam()
           milkboy.say("╚═། ◑ ▃ ◑ །═╝",  280, 280, 10).then(() => {
-            timeout(() => loopBuddy(() => {
-              milkboy.loop = true
-              ctx.resetTransform();
-              milkboy.draw(550, 0)
-              milkboy.mutate(mom)
-            }, 2000, 100).then(() => {
-              milkboy.say("F",  280, 340, 10).then(() => {
-                milkboy.say("YEAH",  300, 340, 10).then(() => {
+                timeout(() => loopBuddy(() => {
+                  milkboy.loop = true
+                  ctx.resetTransform();
+                  milkboy.draw(550, 0)
+                  milkboy.mutate(mom)
+                }, 2000, 100), 1000).then(() => {
+                  mom.say("I'M", 35, 400, 100).then(() => mom.say("...", 60, 400, 400)).then(() => mom.say("DISSAPOINTED IN YOU", 80, 400, 200))
                   loopBuddy(() => {
-                    milkboy.draw(milkboy.posX, milkboy.posY + 10)
-                  }, 8000, 100)
+                    var rand = 20*Math.random()
+                    if(Math.ceil(rand) % 2 === 0)
+                      rand *= -1
+                    milkboy.draw(milkboy.posX + rand, milkboy.posY + 10)
+                  }, 7000, 100).then(() => {
+                    $('.message').remove()
+                    mom.loop = false
+                    mom.run = false
+                    mom = null
+                    milkboy.loop = false
+                    milkboy.run = false
+                    ctx.resetTransform();
+                    ctx.clearRect(0, 0, 10000, 10000);
+                    darkSequence()
+                  })
                 })
               })
-            })
-            , 1000)
-          })
         })
       })
     })
   })
-  /*ctx.translate(600,200);
-  milkboy.draw()
-  timeout(null, 500)
+}
+
+
+async function darkSequence()
+{
+  $('body').addClass('dark')
+  milkboy.dark = true
+  await wait(500)
+  ctx.beginPath();
+  ctx.fillStyle = 'yellow';
+  ctx.arc(200, 400, 200, 0, Math.PI * 2, true); // Outer circle
+  ctx.fill();
+  milkboy.run = true
+  milkboy.draw(-10, 300)
   ctx.resetTransform();
-  ctx.translate(70,200);
-  mom.draw()
-  ctx.resetTransform();
-  ctx.translate(600,200);
+  await loopBuddy(() => {
+    milkboy.run = true
+    milkboy.draw(milkboy.posX + 20, milkboy.posY)
+  }, 1000, 100)
   loopBuddy(() => {
-    ctx.translate(0,2);
-    ctx.clearRect(0, 0, 1000, 1000);
-    milkboy.draw()
-    milkboy.mutate(mom)
-  }, 8000, 100)*/
+    ctx.resetTransform();
+    ctx.clearRect(0, 0, 400, 400);
+    ctx.beginPath();
+    ctx.fillStyle = 'yellow';
+    ctx.arc(200, 400, 200, 0, Math.PI * 2, true); // Outer circle
+    ctx.fill();
+    milkboy.draw(milkboy.posX, milkboy.posY)
+  }, 10000, 100)
+  await wait(500)
+  await milkboy.say("HELLO THERE?", 35, 400, 100)
+  await wait(500)
+  await milkboy.say("IS ANYONE OUT HERE?", 35, 450, 100)
+  await wait(500)
+  await milkboy.say("IT'S ME, MILK JR.", 35, 500, 100)
+  loopBuddy(() => {
+    ctx.resetTransform();
+    ctx.beginPath();
+    ctx.fillStyle = 'yellow';
+    ctx.arc(milkboy.posX + 5, 400, 200, 0, Math.PI * 2, true); // Outer circle
+    ctx.fill();
+    milkboy.draw(milkboy.posX + 10, milkboy.posY)
+  }, 12000, 100)
 }
 
 //momSequence()
 titleSequence()
+//darkSequence()
+
 
 
 
