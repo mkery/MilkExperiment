aef99bb1a630ab687a04a6ddeaed3a84c2e0915e
mkery <mkery@cs.cmu.edu>
2018-01-31 15:08:04 -0500

    yellow sequence complete

--- renderer.js
+++ renderer.js
@@ -17,6 +17,7 @@ ctx.lineWidth = 3;
 ctx.clearRect(0, 0, 1000, 1000);
 
 const milkboy = new Actor(ctx, './images/test_1.svg', 0)
+const milkboy_orig = new Actor(ctx, './images/test_1.svg', 0)
 
 
 function wait(ms) {
@@ -165,24 +166,113 @@ async function darkSequence()
   })
 }
 
-//momSequence()
-//titleSequence()
-//darkSequence()
-yellowSequence()
-
-
 async function yellowSequence()
 {
   $('body').removeClass('dark')
   $('body').addClass('bananna')
+  milkboy.run = true
+  milkboy.draw(-10, 600)
+  ctx.resetTransform();
+  await loopBuddy(() => {
+    milkboy.run = true
+    milkboy.draw(milkboy.posX + 20, milkboy.posY)
+  }, 600, 100)
+
+  var rotate = 0
+  var banannafo = new Actor(ctx, './images/bananna.svg', 5)
   var bananna0 = new Actor(ctx, './images/bananna.svg', 5)
-  bananna0.loop = true
-  bananna0.clearOnDraw = true
-  bananna0.draw(300, 200, 4)
+  loopBuddy(() => {
+    rotate += 5
+    if(rotate % 10 === 0)
+    {
+      rotate *= -1
+      ctx.resetTransform()
+      ctx.clearRect(0, 0, 10000, 10000);
+    }
+    milkboy.draw(milkboy.posX, milkboy.posY)
 
+    bananna0.draw(300, 200, 3, 30*rotate)
 
+    var bananna1 = new Actor(ctx, './images/bananna.svg', 5)
+    bananna1.draw(400, 50, 4, -30*rotate)
+
+    var bananna2 = new Actor(ctx, './images/bananna.svg', 5)
+    bananna2.draw(600, 400, 2, 70*rotate)
+
+    var bananna22 = new Actor(ctx, './images/bananna.svg', 5)
+    bananna22.draw(400, 400, 2, 70*rotate)
+
+    var bananna3 = new Actor(ctx, './images/bananna.svg', 5)
+    bananna3.draw(800, 700, 5, 120*rotate)
+
+    var bananna4 = new Actor(ctx, './images/bananna.svg', 5)
+    bananna4.draw(600, 800, 3, -30*rotate)
+  }, 12000, 600)
+
+  milkboy.dark = true
+  await wait(1000)
+  await milkboy.say("WOW! A BANANNA PARTY!", 35, 450, 100)
+  await wait(500)
+  $('.message').remove()
+  await milkboy.say("IT'S .... (つ°ヮ°)つ ....", 35, 450, 100)
+  await loopBuddy(() => {
+    milkboy.clearOnDraw = true
+    ctx.resetTransform();
+    milkboy.draw(milkboy.posX, milkboy.posY)
+    milkboy.mutate(banannafo)
+  }, 2000, 100)
+  milkboy.say("BANANNA TIME!", 35, 500, 100)
+  await loopBuddy(() => {
+    rotate += 5
+    if(rotate % 10 === 0)
+    {
+      rotate *= -1
+      ctx.resetTransform()
+    }
+    milkboy.draw(milkboy.posX, milkboy.posY, 1, 30*rotate)
+  }, 4000, 200)
+  $('.message').remove()
+  bananna0.dark = true
+  await bananna0.say("DUDE WTF", 300, 200, 100)
+  await wait(500)
+  await bananna0.say("YOU'RE RUINING THE VIBE", 300, 300, 80)
+  await wait(800)
+  $('.message').remove()
+  await bananna0.say("ARE YOU TRYING TO BE A BANANNA?", 300, 200, 40)
+  await wait(500)
+  await bananna0.say("THAT'S PRETTY INSULTING TO OUR CULTURE", 250, 280, 40)
+  await wait(500)
+  await milkboy.say("OH", 35, 450, 100)
+  await loopBuddy(() => {
+    milkboy.clearOnDraw = true
+    ctx.resetTransform();
+    milkboy.draw(milkboy.posX, milkboy.posY)
+    milkboy.mutate(milkboy_orig)
+  }, 4000, 100)
+  milkboy.oDOM = milkboy_orig.oDOM
+  milkboy.loop = true
+  milkboy.draw(milkboy.posX, milkboy.posY)
+  $('.message').remove()
+  await milkboy.say("I'M SORRY YOU GUYS", 35, 450, 100)
+  await milkboy.say("I'M STILL AN INEPT ALGORITHM", 250, 450, 80)
+  milkboy.say("... I'LL BE GOING NOW ... ... ... ...", 35, 510, 200)
+  await loopBuddy(() => {
+    milkboy.clearOnDraw = false
+    ctx.resetTransform();
+    ctx.beginPath();
+    ctx.fillStyle = 'pink';
+    ctx.arc(milkboy.posX + 5, 500, 100, 0, Math.PI * 2, true); // Outer circle
+    ctx.fill();
+    ctx.resetTransform();
+    milkboy.draw(milkboy.posX + 10, milkboy.posY)
+  }, 10000, 100)
 }
 
+
+//momSequence()
+//titleSequence()
+//darkSequence()
+yellowSequence()
 
 
 /*var milkboy = new Actor(ctx, './images/test_1.svg')
