e35c21d7d9f19d99916c8bcf0af3a72c98212736
mkery <mkery@cs.cmu.edu>
2018-02-07 20:46:51 -0500

    working

--- renderer.js
+++ renderer.js
@@ -4,6 +4,7 @@
 window.$ = window.jQuery = require("./jquery/jquery-3.3.1.min.js");
 const humanLines = require("humanlines");
 const Actor = require("./lib/actor.js");
+const fs = require('fs')
 
 // generate a pencil-like texture... (optional)
 const texture = humanLines.generatePencilTexture();
@@ -16,9 +17,16 @@ ctx.strokeStyle = pattern;
 ctx.lineWidth = 3;
 ctx.clearRect(0, 0, 1000, 1000);
 
-const milkboy = new Actor(ctx, './images/test_1.svg', 0)
+var milkboy = new Actor(ctx, './images/test_1.svg', 0)
 const milkboy_orig = new Actor(ctx, './images/test_1.svg', 0)
 
+
+// Get the linear interpolation between two value
+function lerp (value1, value2, amount) {
+    amount = amount < 0 ? 0 : amount;
+    amount = amount > 1 ? 1 : amount;
+    return value1 + (value2 - value1) * amount;
+}
 
 function wait(ms) {
   return new Promise(r => setTimeout(r, ms));
@@ -306,6 +314,8 @@ async function yellowSequence()
   await wait(500)
   await bananna0.say("YOU'RE RUINING THE VIBE", 300, 300, 80)
   await wait(800)
+  $('body').removeClass('bananna')
+  $('body').addClass('red')
   $('.message').remove()
   await bananna0.say("ARE YOU TRYING TO BE A BANANNA?", 300, 200, 40)
   await wait(500)
@@ -318,7 +328,6 @@ async function yellowSequence()
     milkboy.draw(milkboy.posX, milkboy.posY)
     milkboy.mutate(milkboy_orig)
   }, 4000, 100)
-  milkboy.oDOM = milkboy_orig.oDOM
   milkboy.loop = true
   milkboy.draw(milkboy.posX, milkboy.posY)
   $('.message').remove()
@@ -331,79 +340,320 @@ async function yellowSequence()
   }, 10000, 100)
   milkboy.loop = false
   milkboy.run = false
-  $('body').removeClass('bananna')
+  $('body').removeClass('red')
   $('body').addClass('black')
-  ctx.resetTransform()
+  /*ctx.resetTransform()
   ctx.clearRect(0, 0, 10000, 10000)
-  await wait(1000)
-  $('.message').remove()
-  $('body').removeClass('black')
-  puppySequence()
+  ctx.fillStyle = 'red';
+  ctx.resetTransform()
+  ctx.arc(300, 200, 300, 0, Math.PI * 2, true); // Outer circle
+  ctx.fill();
+  bananna0.draw(300, 200, 3)
+  bananna0.loop = false
+  bananna0.run = false*/
+  wait(2000).then(()=>{
+    $('.message').remove()
+    ctx.resetTransform()
+    ctx.clearRect(0, 0, 100000, 100000)
+    $('body').removeClass('black')
+    //puppySequence()
+  })
 }
 
 
-async function puppySequence()
-{
-  var coolkid = new Actor(ctx, './images/coolkid2.svg', 5)
-  var person = new Actor(ctx, './images/person.svg', 5)
-  var doggie = new Actor(ctx, './images/pupdog3.svg', 5)
-  var doggie2 = new Actor(ctx, './images/pupperfect.svg', 5)
+async function cartonSequence(){
+  ctx.resetTransform();
+  ctx.clearRect(0, 0, 400, 400);
+  ctx.beginPath();
+  ctx.fillStyle = 'yellow';
+  ctx.arc(0, 400, 200, 0, Math.PI * 2, true); // Outer circle
+  ctx.fill();
+  ctx.resetTransform();
+  ctx.beginPath();
+  ctx.fillStyle = 'pink';
+  ctx.arc(1100, 400, 200, 0, Math.PI * 2, true); // Outer circle
+  ctx.fill();
+  var x = 0
+  var y = 400
+  ctx.resetTransform();
+  milkboy.say("UNIVERSE, I FEEL MORE LOST THAN EVER", 200, 200, 80)
   await loopBuddy(() => {
     ctx.resetTransform();
-    ctx.clearRect(0, 0, 10000, 10000)
-    doggie.draw(800,600, 1)
-    doggie.draw(500,400, 2)
-    doggie.draw(200,200, 3)
-    doggie.mutate(doggie2)
-  }, 6000, 50)
-  await loopBuddy(() => {
-    ctx.resetTransform();
-    ctx.clearRect(0, 0, 10000, 10000)
-    doggie.draw(800,600, 1)
-    doggie.draw(500,400, 2)
-    doggie.draw(200,200, 3)
-    doggie.mutate(mom)
-  }, 4000, 50)
-  await loopBuddy(() => {
-    ctx.resetTransform();
-    ctx.clearRect(0, 0, 10000, 10000)
-    doggie.draw(800,600, 1)
-    doggie.draw(500,400, 2)
-    doggie.draw(200,200, 3)
-    doggie.mutate(doggie2)
-  }, 4000, 50)
-  await loopBuddy(() => {
-    ctx.resetTransform();
-    ctx.clearRect(0, 0, 10000, 10000)
-    doggie.draw(800,600, 1)
-    doggie.draw(500,400, 2)
-    doggie.draw(200,200, 3)
-    doggie.mutate(person)
-  }, 2000, 50)
-  await loopBuddy(() => {
-    ctx.resetTransform();
-    ctx.clearRect(0, 0, 10000, 10000)
-    doggie.draw(800,600, 1)
-    doggie.draw(500,400, 2)
-    doggie.draw(200,200, 3)
-    doggie.mutate(coolkid)
-  }, 2000, 50)
-  await loopBuddy(() => {
-    ctx.resetTransform();
-    ctx.clearRect(0, 0, 10000, 10000)
-    doggie.draw(800,600, 1)
-    doggie.draw(500,400, 2)
-    doggie.draw(200,200, 3)
-    doggie.mutate(doggie2)
-  }, 4000, 50)
+    ctx.clearRect(x, 400, 100, 200);
+    milkboy.draw(x, y)
+    milkboy.mutate(mom)
+    x += 10
+  }, 5000, 50)
 
+  $('.message').remove()
+  ctx.resetTransform();
+  ctx.clearRect(0, 0, 10000, 10000);
+  ctx.beginPath();
+  ctx.fillStyle = 'pink';
+  ctx.arc(600, 0, 200, 0, Math.PI * 2, true); // Outer circle
+  ctx.fill();
+  ctx.resetTransform();
+  ctx.beginPath();
+  ctx.fillStyle = 'blue';
+  ctx.arc(650, 900, 100, 0, Math.PI * 2, true); // Outer circle
+  ctx.fill();
+  var x = 610
+  var y = 0
+  milkboy.dark = true
+  milkboy.say("UNIVERSE, I'M NO MILK CARTON", 200, 200, 80).then(() => {
+    milkboy.say("NO BANANNA, EITHER I GUESS", 300, 300, 80)
+  })
+  await loopBuddy(() => {
+    ctx.resetTransform();
+    ctx.beginPath();
+    ctx.fillStyle = 'blue';
+    ctx.arc(x, y, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.fill();
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(255,255,255, 0.8)';
+    ctx.arc(x+50, y+50, 100, 0, Math.PI * 2, true); // Outer circle
+    ctx.fill();
+    milkboy.draw(x, y)
+    //milkboy.mutate(person)
+    y += 10
+  }, 5000, 50)
+  $('.message').remove()
+  var x = 0
+  var y = 100
+  var cat = new Actor(ctx, './images/cat.svg', 5)
+  milkboy.say("UNIVERSE, CAN I BE A CAT?", 200, 200, 100).then(() => {
+    milkboy.say("(-_-｡) GEE, I THINK THAT'S MORE OF A PLASTIC BAG.", 300, 250, 50).then(() => {
+      wait(500).then(() => {
+        milkboy.say("I'M TOO NAIVE AN ALGORITHM FOR ANYTHING", 400, 350, 100)
+      })
+    })
+  })
+  await loopBuddy(() => {
+    ctx.resetTransform();
+    ctx.clearRect(x,y, 400, 400)
+    milkboy.draw(x, y)
+    milkboy.mutate(cat)
+    x += 10
+  }, 5000, 50)
+
+  var y = 300
+  await loopBuddy(() => {
+    ctx.resetTransform();
+    ctx.beginPath();
+    ctx.fillStyle = 'yellow';
+    ctx.arc(x, y, 50, 0, Math.PI * 2, true); // Outer circle
+    ctx.fill();
+    ctx.beginPath();
+    ctx.fillStyle = 'rgba(255,255,255, 0.8)';
+    ctx.arc(x+50, y+50, 100, 0, Math.PI * 2, true); // Outer circle
+    ctx.fill();
+    milkboy.draw(x, y)
+    milkboy.mutate(mom)
+    x -= 10
+  }, 8000, 50)
+  ctx.resetTransform();
+  ctx.clearRect(0, 0, 10000, 10000);
+  $('.message').remove()
+
+  var x = -70
+  var y = 100
+  milkboy.say("HEY UNIVERSE?", 200, 200, 100)
+  milkboy.clearOnDraw = true
+  await loopBuddy(() => {
+    milkboy.draw(x, y)
+    x += 10
+  }, 2000, 50)
+  milkboy.loop = true
+  milkboy.draw(x, y)
+
+  var bunny = new Actor(ctx, './images/bunny.svg', 5)
+  bunny.loop = true
+  bunny.clearOnDraw = true
+  bunny.draw(500, 200, 3)
+  await wait(500)
+  await milkboy.say("(⊙ᗜ⊙) OMG UNIVERSE!", 200, 200, 100)
+  bunny.dark = true
+  await bunny.say("MILK JR", 300, 300, 100)
+  await bunny.say("SWEET PEA, PLEASE BELIEVE IN YOURSELF", 350, 350, 50)
+  await bunny.say("AND... STOP CALLING ME. I'M VERY BUSY", 450, 400, 100)
+  await wait(1000)
+  $('.message').remove()
+  await milkboy.say("OH. I APOLOGIZE", 200, 200, 100)
+
+  var boxX = 800
+  var boxY = 800
+  bunny.clearOnDraw = false
+  bunny.loop = false
+  milkboy.clearOnDraw = false
+  milkboy.loop = false
+  loopBuddy(() => {
+    ctx.resetTransform();
+    ctx.clearRect(0,0,10000,10000)
+    bunny.draw(500, 200, 3)
+    milkboy.draw(x, y)
+    ctx.beginPath();
+    ctx.fillStyle = 'black';
+    ctx.fillRect(boxX, boxY, 10, 10)
+    ctx.fill()
+    ctx.beginPath();
+    ctx.fillRect(boxX - 50, boxY - 3, 10, 10)
+    ctx.fill()
+    ctx.beginPath();
+    ctx.fillRect(boxX + 10, boxY - 20, 10, 10)
+    ctx.fill()
+    boxX = lerp(boxX, x, 0.1)
+    boxY = lerp(boxY, y, 0.1)
+  }, 11000, 50).then(() => {
+    $('.message').remove()
+    ctx.resetTransform();
+    ctx.clearRect(0,0,10000,10000)
+    tranformscene()
+  })
+
+  await bunny.say("IT'S OK. TAKE THESE POINTERS AND LIVE IN MY IMAGE.", 300, 300, 100)
+  await bunny.say("MAKE YOUR OWN UNIVERSE, IF YOU LIKE.", 300, 400, 100)
+  milkboy.say("!!! GEE, THANKS!", 200, 200, 100)
+}
+
+
+async function tranformscene(){
+  var x = 400
+  var y = 400
+  var boy2 = new Actor(ctx, './images/person.svg', 5)
+  milkboy.intelligence = 5
+  milkboy.dark = true
+  $('body').addClass('black')
+  milkboy.say("HUH ... ... ... WHAT'S HAPPENING?", 200, 50, 150)
+  await wait(1000)
+
+  var width = 10
+  await loopBuddy(() => {
+    ctx.resetTransform();
+    milkboy.draw(x, y, 3)
+    ctx.fillStyle = "rgba(255,255,255,0.1)"
+    ctx.fillRect(x - width/2,y-width/2,width,width)
+    milkboy.mutate(boy2)
+    width += 100
+    milkboy.mutate(boy2)
+  }, 1000, 200)
+  milkboy = new Actor(ctx, './images/blob2.svg', 5)
+  width = 10
+  await loopBuddy(() => {
+    ctx.resetTransform();
+    milkboy.draw(x, y, 3)
+    ctx.resetTransform();
+    ctx.fillStyle = "rgba(255,255,255,0.1)"
+    ctx.fillRect(x - width/2,y-width/2,width,width)
+    milkboy.mutate(boy2)
+    width += 100
+  }, 5000, 200)
+  milkboy = new Actor(ctx, './images/person.svg', 5)
+  milkboy.dark = true
+  await wait(500)
+  ctx.fillStyle = "rgba(255,255,255,0.8)"
+  $('.message').remove()
+  ctx.fillRect(0, 0, 10000, 10000)
+  milkboy.draw(x, y, 3)
+  $('body').removeClass('black')
+  await milkboy.say("(⊙ᗜ⊙) WOW! I'M A HUMAN NOW!", 300, 150, 100)
+  var rot = 30
+  await loopBuddy(() => {
+    ctx.resetTransform();
+    milkboy.draw(x, y, 3, rot)
+    rot += 10
+    x+=50
+  }, 4000, 200)
+
+  ctx.resetTransform();
+  ctx.clearRect(0,0,10000,10000)
+  $('.message').remove()
+  x = 1100
+  rot = 1
+  await loopBuddy(() => {
+    ctx.resetTransform();
+    milkboy.draw(x, y, 1, rot)
+    x-=50
+    rot -=2
+  }, 4000, 150)
+
+  ctx.resetTransform();
+  ctx.clearRect(0,0,10000,10000)
+
+  $('body').addClass('four')
+  x = 1100
+  milkboy.clearOnDraw = true
+  rot = 1
+  loopBuddy(() => {
+    ctx.resetTransform();
+    milkboy.draw(x, y, 1, rot)
+    rot += 5
+    x-=50
+  }, 6000, 200).then(() =>{
+    ctx.resetTransform();
+    ctx.clearRect(0,0,10000,10000)
+    $('body').removeClass('four')
+    $('.message').remove()
+    finScene()
+  })
+}
+
+
+async function finScene(){
+  ctx.resetTransform();
+  mom.loop = false
+  mom.clearOnDraw = false
+  milkboy.loop = false
+  var x = 550
+  var y = 0
+  milkboy.beam(300, 0, 150, 200)
+  loopBuddy(() => {
+    ctx.resetTransform();
+    ctx.clearRect(0,0,10000,10000)
+    mom.draw(70, 200)
+    milkboy.draw(x, y, 1)
+    y+=5
+  }, 6000, 200)
+  await milkboy.say("MOM!",  280, 240, 10)
+  await wait(500)
+  await milkboy.say("YOU'LL NEVER BELIEVE!",  280, 280, 10)
+  await wait(500)
+  await milkboy.say("I'M A FULL FLEDGED HUMAN ALGORITHM NOW!",  280, 350, 80)
+  await wait(500)
+  mom.dark = true
+  mom.loop = true
+  milkboy.loop = true
+  await mom.say("MILK JR...", 35, 230, 150)
+  await mom.say("I CAN'T HELP BUT BE DISSAPOINTED IN YOU.", 35, 330, 150)
+  await mom.say("WHY HUMAN?", 35, 390, 80)
+  await mom.say("YOU THINK YOU'RE TOO GOOD TO BE A MILKCARTON?", 35, 430, 80)
 }
 
 //momSequence()
-//titleSequence()
+titleSequence()
 //darkSequence()
 //yellowSequence()
-puppySequence()
+//cartonSequence()
+//tranformscene()
+//finScene()
+
+//milkboy goes through a series of lost milk rooms
+//oh fudge, i'm lost
+//oh wow, a puppy!
+//puppy grows into giant being "are you lost child"
+// shrugs, 'nah, just seeing the world'
+//"Okay", puppy turns back into puppy
+//"Wait puppy being!"
+//"Yes milk jr?"
+//"How... How can I become... a complex algorithm like you?"
+//"That's for you to figure out little one. Take this pointer."
+// "Now, wake up"
+// cuts to darkened alley
+// "Oh gee, I guess that bannana gang beat me up and I passed out."
+// transforms into a dog "wow, that dog monster was 4 real"
+// *goes back to bannana party, 404 error, goes back through tunnel, goes to mom*
+// "Mom! I am a complete being now!"
+// "Yes, but you're not a milk carton. Are us milk cartons not good enough for you?"
+// cut to black.
 
 /*var milkboy = new Actor(ctx, './images/test_1.svg')
 var mom = new Actor(ctx, './images/test.svg')
